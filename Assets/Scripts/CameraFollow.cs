9f06698654132c9cb096a0cd331b01e3967d6f51
Anabelle Mandel <anabelle.world@yahoo.com>
2022-10-24 17:00:35 -0400

    fixed camera and rect of icons

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -15,15 +15,18 @@ public class CameraFollow : MonoBehaviour
         boundsMin = new Vector2(-7.8f, -20.7f);
         boundsMax = new Vector2(16.7f, 6.3f);
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
     }
 
     void Update()
     {
         if (player.position.x > boundsMin.x && player.position.x < boundsMax.x)
         {
-            if (player.position.y > boundsMin.y && player.position.y < boundsMax.y){
-                transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
-            }
+            transform.position = new Vector3(player.position.x + offset.x, transform.position.y, transform.position.z);
+        }
+        if (player.position.y > boundsMin.y && player.position.y < boundsMax.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y + offset.y, transform.position.z);
         }
     }
 }
